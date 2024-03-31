a426a2c607589dfd6e1224152b00d24be3464bb5
v0ltmx <lucas2015.gabrill@hotmail.com>
2024-03-31 18:24:28 -0300

    Adicionando carousel no sensation

--- src/Interfaces/Sensation/index.tsx
+++ src/Interfaces/Sensation/index.tsx
@@ -1,149 +1,217 @@
-import { Card, Modal, Table, Tag } from "antd";
+import { Button, Card, Carousel, Modal, Table, Tag } from "antd";
 import { useState } from "react";
 
 export default function Sensation() {
-
-  const [visible, setVisible] = useState(false); 
+  const [visible, setVisible] = useState(false);
   const [modalImage, setModalImage] = useState("");
+  const [carouselImages, setCarouselImages] = useState<string[]>([]);
+  const [visibleCarousel, setVisibleCarousel] = useState(false);
 
   const { Meta } = Card;
 
   const mapKeyToColor = (key: any) => {
     switch (key) {
-      case '1':
-        return 'red';
-      case '2':
-        return 'pink';
-  
-     
+      case "1":
+        return "red";
+      case "2":
+        return "pink";
+
       default:
-        return 'blue'; 
+        return "blue";
     }
   };
 
   const dataSource = [
     {
-      key: '1',
-      name: 'Person',
-      rank: 'Leader',
+      key: "1",
+      name: "Person",
+      rank: "Leader",
     },
     {
-      key: '2',
-      name: 'Cesar_',
-      rank: 'Co-leader',  
+      key: "2",
+      name: "Cesar_",
+      rank: "Co-leader",
     },
     {
-      key: '3',
-      name: 'f800cardoso',
-      rank: 'Member',  
+      key: "3",
+      name: "f800cardoso",
+      rank: "Member",
     },
     {
-      key: '4',
-      name: 'Rodz',
-      rank: 'Member',  
+      key: "4",
+      name: "Rodz",
+      rank: "Member",
     },
     {
-      key: '5',
-      name: 'Angyy',
-      rank: 'Member',  
+      key: "5",
+      name: "Angyy",
+      rank: "Member",
     },
     {
-      key: '6',
-      name: 'Zarkthenberg',
-      rank: 'Member',  
+      key: "6",
+      name: "Zarkthenberg",
+      rank: "Member",
     },
     {
-      key: '7',
-      name: 'Historic',
-      rank: 'Member',  
+      key: "7",
+      name: "Historic",
+      rank: "Member",
     },
   ];
-  
+
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
     },
     {
-      title: 'Rank',
-      dataIndex: 'rank',
-      key: 'rank',
+      title: "Rank",
+      dataIndex: "rank",
+      key: "rank",
       render: (text: any, record: any) => (
         <Tag color={mapKeyToColor(record.key)}>{text}</Tag>
       ),
     },
-   
   ];
 
   const cardsData = [
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 18',
-      cover: 'https://r2.easyimg.io/2tnzihlxt/whatsapp_image_2024-03-28_at_20.40.28.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 18",
+      cover:
+        "https://r2.easyimg.io/2tnzihlxt/whatsapp_image_2024-03-28_at_20.40.28.jpeg",
     },
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 7',
-      cover: 'https://r2.easyimg.io/psi2392i8/whatsapp_image_2024-03-24_at_21.35.51.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 7",
+      cover:
+        "https://r2.easyimg.io/psi2392i8/whatsapp_image_2024-03-24_at_21.35.51.jpeg",
     },
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 19',
-      cover: 'https://r2.easyimg.io/z6z9s6lmv/whatsapp_image_2024-03-28_at_13.26.23.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 19",
+      cover:
+        "https://r2.easyimg.io/z6z9s6lmv/whatsapp_image_2024-03-28_at_13.26.23.jpeg",
     },
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 14',
-      cover: 'https://r2.easyimg.io/9jvs9fpnd/whatsapp_image_2024-03-28_at_13.31.11.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 14",
+      cover:
+        "https://r2.easyimg.io/9jvs9fpnd/whatsapp_image_2024-03-28_at_13.31.11.jpeg",
     },
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 10',
-      cover: 'https://r2.easyimg.io/pi318u3uc/whatsapp_image_2024-03-26_at_20.45.20.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 10",
+      cover:
+        "https://r2.easyimg.io/pi318u3uc/whatsapp_image_2024-03-26_at_20.45.20.jpeg",
     },
     {
-      title: 'Sensation x Excalibur',
-      description: '20 a 19',
-      cover: 'https://r2.easyimg.io/apw67q0qz/whatsapp_image_2024-03-27_at_20.21.34.jpeg',
+      title: "Sensation x Excalibur",
+      description: "20 a 19",
+      cover:
+        "https://r2.easyimg.io/apw67q0qz/whatsapp_image_2024-03-27_at_20.21.34.jpeg",
     },
-   
   ];
 
   const handleCardClick = (cover: string) => {
-    setModalImage(cover); 
+    setModalImage(cover);
     setVisible(true);
   };
-    return <>
+
+  const handleVerTodasFotosClick = () => {
+    const allImages = cardsData.map((card) => card.cover);
+    setCarouselImages(allImages);
+    setVisibleCarousel(true);
+  };
+
+  const handleCloseCarousel = () => {
+    setVisibleCarousel(false);
+    setCarouselImages([]);
+  };
+
+  return (
+    <>
       <h2>Sensation</h2>
       <Table dataSource={dataSource} columns={columns} />;
-
       <div>
-      <h2>Histórico de vitórias</h2>
-  </div>
+        <h2>Histórico de vitórias</h2>
+      </div>
 
-  <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(270px, 1fr))', gap: '10px' }}>
-  {cardsData.map((card, index) => (
+      <div
+        style={{
+          display: visibleCarousel ? "flex" : "block",
+          gap: visibleCarousel ? "10px" : "none",
+          marginBottom: "40px",
+        }}
+      >
+        <Button onClick={handleVerTodasFotosClick}>Ver todas as fotos</Button>
+        {visibleCarousel && (
+          <>
+            <Button type="primary" onClick={handleCloseCarousel}>Fechar</Button>
+          </>
+        )}
+      </div>
+
+      {visibleCarousel && (
+        <>
+          <Carousel dotPosition="top" effect="scrollx">
+            {carouselImages.map((image: string, index: number) => (
+              <div key={index}>
+                <img
+                  src={image}
+                  alt="Imagem"
+                  style={{
+                    maxWidth: "1024px",
+                    display: "block",
+                    margin: "0 auto",
+                    marginBottom: "-100px",
+                  }}
+                />
+              </div>
+            ))}
+          </Carousel>
+        </>
+      )}
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(270px, 1fr))",
+          gap: "10px",
+        }}
+      >
+        {cardsData.map((card, index) => (
           <Card
             key={index}
             hoverable
-            style={{ width: 280, background:'#dedede', margin: '10px' }}
-            cover={<img alt="example" src={card.cover} style={{ width: '100%', height: '200px', objectFit: 'cover' }} onClick={() => handleCardClick(card.cover)} />}
+            style={{ width: 280, background: "#dedede", margin: "10px" }}
+            cover={
+              <img
+                alt="example"
+                src={card.cover}
+                style={{ width: "100%", height: "200px", objectFit: "cover" }}
+                onClick={() => handleCardClick(card.cover)}
+              />
+            }
           >
             <Meta title={card.title} description={card.description} />
           </Card>
         ))}
-  </div>
-
-  <Modal
+      </div>
+      <Modal
         title="Imagem da CW"
         visible={visible}
-        onCancel={() => setVisible(false)} 
+        onCancel={() => setVisible(false)}
         footer={null}
         width={1200}
       >
-        <img src={modalImage} alt="Imagem em tamanho maior" style={{ maxWidth: '1024px', display: 'block', margin: '0 auto' }} />
+        <img
+          src={modalImage}
+          alt="Imagem em tamanho maior"
+          style={{ maxWidth: "1024px", display: "block", margin: "0 auto" }}
+        />
       </Modal>
-    </>;
-  }
-  
+    </>
+  );
+}
